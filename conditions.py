d9327b13318419ffd95d08a834b06f8fd675fb23
SouvikPal001 <er.souvikpal@gmail.com>
2025-09-08 22:51:00 +0530

    Demonstration file named 'conditions.py' added for conditional statements in python

---
+++ conditions.py
@@ -0,0 +1,41 @@
+#Conditions in Python:
+#Conditional Operators: ==,!=,>,<,>=,<=
+#Conditional Statements: if, if-else, if-elif-else, nested if-elif-else
+#Note: In python, indentation is important because {} aren't used to indicate where the block of code belongs
+
+
+# Program to demonstrate all conditional statements
+
+# Take an integer input
+n = int(input("Enter a number: "))
+
+# --- Simple if ---
+if n > 0:
+    print("The number is positive.")
+
+# --- if-else ---
+if n % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+# --- if-elif-else ---
+if n == 0:
+    print("The number is zero.")
+elif n > 0:
+    print("The number is greater than zero.")
+else:
+    print("The number is less than zero.")
+
+# --- Nested if ---
+if n != 0:
+    if n > 1:
+        print("The number is greater than one.")
+    else:
+        print("The number is either 0 or 1.")
+else:
+    print("The number is exactly zero.")
+
+print("Conditional Statements Demonstration Completed.")
+
+#Thank you!
